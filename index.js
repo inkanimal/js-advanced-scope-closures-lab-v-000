e71bd7aa363a3f0a9ebfb98ca1e0efbed872bcd8
geoffrey woodcock <theatomproject@gmail.com>
2019-05-29 01:55:52 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,6 @@
+function produceDrivingRange(block){
+  return function(start, end){
+    let diff = Math.abs(parseInt(end.slice(0,2)) - parseInt(start.slice(0,2)));
+    return (diff > block)? `${ diff - block } blocks out of range` : `within range by ${ block - diff }`;
+  }
+}
